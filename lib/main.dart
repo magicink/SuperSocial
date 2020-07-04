572c4d23e6ed06aeb6dcb83316da162120343dca
Brandon Tom <magicink@gmail.com>
2020-07-04 10:10:02 -0700

    1.0.12 (#10)
    * Section 11: Liking and Unliking Posts
    
    * Section 11: Liking and Unliking Posts (#7) (#8)
    
    * Section 12: Real-time messaging with comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:SuperSocial/pages/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'pages/home.dart';
 
 void main() {
   runApp(MyApp());
